9ca20091c9efdda73000033c3801ac1ddaf7b2e1
Thong Hoang <133630383+thonghoangdanh@users.noreply.github.com>
2023-10-06 21:49:06 +0300

    [BE-2] database model
    * migration file for the  database

---
+++ resources/db/migration/nuke.sql
@@ -0,0 +1,10 @@
+-- TODO: tool to nuke the db
+
+DROP TABLE company;
+DROP TABLE user_role;
+DROP TABLE user;
+DROP TABLE story_status;
+DROP TABLE story;
+DROP TABLE comment;
+DROP TABLE tag;
+DROP TABLE story_tag;
